a102e2d84e463ef097769ab40143a757ac158a42
chezzy577-hub <chezzy577@gmail.com>
2026-03-22 07:29:17 +0800

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,18 +1,19 @@
 <?php
-$host     = "sql106.infinityfree.com";
-$dbname   = "if0_40551884_smart_power";
-$username = "if0_40551884";
-$password = "smarthomethesis";
+$host     = "dpg-d6vigrhr0fns73cad3q0-a";
+$port     = "5432";
+$dbname   = "smart_power";
+$username = "smartpower_user";
+$password = "EyLoWqbVYTnDxndaRmsbHispohU0XksG";
 
 try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $dsn,
         $username,
         $password,
         [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
         ]
     );
 } catch (Throwable $e) {
